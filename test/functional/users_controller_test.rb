4670c2b72f3138c15b929926d4171be221d4ec2d
Eric Hill <eric@many9s.com>
2012-11-02 14:42:59 -0700

    Removing unused tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -9,27 +9,6 @@ class UsersControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
-
-  test "should update user" do
-    put :update, id: @user, user: {  }
-    assert_redirected_to user_path(assigns(:user))
   end
 
 end
