c7bb615e04878a6b99a31c6241c3dc3256ba8e49
Eric Hill <eric@many9s.com>
2012-11-02 14:40:46 -0700

    gradual engagement means passwords need to be nullable at first

--- db/schema.rb
+++ db/schema.rb
@@ -22,12 +22,12 @@ ActiveRecord::Schema.define(:version => 20121028015846) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "crypted_password",    :default => "",    :null => false
-    t.string   "password_salt",       :default => "",    :null => false
-    t.string   "email",               :default => "",    :null => false
-    t.string   "persistence_token",   :default => "",    :null => false
-    t.string   "single_access_token", :default => "",    :null => false
-    t.string   "perishable_token",    :default => "",    :null => false
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "email",                                  :null => false
+    t.string   "persistence_token",                      :null => false
+    t.string   "single_access_token",                    :null => false
+    t.string   "perishable_token",                       :null => false
     t.integer  "login_count",         :default => 0,     :null => false
     t.integer  "failed_login_count",  :default => 0,     :null => false
     t.datetime "last_request_at"
